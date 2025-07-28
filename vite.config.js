ab5cfb1b8e0e2a5ad576ec0c2f879a6e9630af8d
ilkerthedev <ilkerselimen@gmail.com>
2025-07-28 20:18:36 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/cinemania-project/',
+  
   root: './',
+  
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
   },
+  
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
+    host: true  
+  },
+  
+  preview: {
+    port: 4173,
+    open: true
   }
 })
