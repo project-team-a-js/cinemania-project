b249d7a3bc4f49ed4f08c1c57d2113a47134ae38
ilkerthedev <ilkerselimen@gmail.com>
2025-08-02 03:08:24 +0300

    src/

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@ import './js/search-list.js';
 import './js/my-movies.js';
 import './js/up-coming.js';
 import './js/weekly-trends.js';
+
+
+/* GitHub Pages için link düzeltmesi */
+const isGitHubPages = window.location.hostname.includes('github.io');
+const basePath = isGitHubPages ? '/cinemania-project' : '';
+
+document.querySelectorAll('a[href^="/"]').forEach(link => {
+  const href = link.getAttribute('href');
+  link.setAttribute('href', basePath + href);
+});
+
+document.querySelectorAll('object[data^="/"]').forEach(obj => {
+  const data = obj.getAttribute('data');
+  obj.setAttribute('data', basePath + data);
+});
