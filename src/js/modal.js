a5c3d4b7a59b9686f1830845266f60789bd913dc
Muhammet Mehmet Öndüç <mehmetonducx@hotmail.com>
2025-08-05 14:24:44 +0300

    in Modal added also Scroll and fixed others. Responsive show's are done on Modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -58,7 +58,8 @@ export function openMovieModal(movie) {
   }
   document.getElementById("modal-genres").textContent = genresText;
   document.getElementById("modal-overview").textContent =
-    movie.overview || "No overview available.";
+    movie.overview ||
+    "No overview available. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.";
   const addToLibraryBtn = document.querySelector(".add-to-library-btn");
   const isInLibrary = isMovieIdInLibrary(movie.id);
   if (isInLibrary) {
